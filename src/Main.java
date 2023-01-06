8b799747ab9655b27ef8651772df5502e9702380
Prasenjeet Urunkar <praurunk@publicisgroupe.net>
2023-01-06 11:41:10 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int randomA, randomB;
+        Scanner takeInput = new Scanner(System.in);
+        System.out.println("Enter 1st no to store in A");
+        randomA = takeInput.nextInt();
+        System.out.println("Enter 1st no to store in B");
+        randomB = takeInput.nextInt();
+        randomA = randomA+randomB;
+        randomB = randomA-randomB;
+        System.out.println("Value in B is"+randomB);
+        randomA=randomA-randomB;
+        System.out.println("Value in A is"+randomA);
+
+
+    }
+}
